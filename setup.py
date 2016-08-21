56d3ff7ce428c45d862acb763df1df19bb40bba3
koallen <weifengzi2009@gmail.com>
2016-08-21 12:20:11 +0800

    upload package to PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = "ntu_course_planner",
+    packages = ["ntu_course_planner"],
+    version = "1.0.0",
+    description = "A course planner for NTU students",
+    author = "Liu Siyuan",
+    author_email = "weifengzi2009@gmail.com",
+    url = "https://github.com/koallen/ntu-course-planner-cli",
+    download_url = "https://github.com/koallen/ntu-course-planner-cli/tarball/v1.0.0",
+    keywords = ["course", "planner", "NTU"]
+)

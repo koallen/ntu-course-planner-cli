693970e82444301ff2c25b4c1a87360c61c90af9
koallen <weifengzi2009@gmail.com>
2017-05-18 13:55:51 +0800

    update to 17/18 sem1, add version checking

--- ntu_course_planner/main.py
+++ ntu_course_planner/main.py
@@ -1,4 +1,13 @@
 from .planner import Planner
+from .version import VERSION
+
+import requests
 
 def main():
-    Planner().start()
+    # make sure the user has the newest course schedule
+    r = requests.get("https://raw.githubusercontent.com/koallen/ntu-course-planner-cli/master/LATEST")
+    if r.text == VERSION:
+        Planner().start()
+    else:
+        print("Please update ntu-course-planner to get the latest course schedule")
+        exit(-1)
